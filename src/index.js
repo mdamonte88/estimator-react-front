03659d42c6e80f9fe8e96fdba922ecf01a0f2c1f
Matias Damonte <mdamonte88@hotmail.com>
2020-09-09 18:44:20 -0300

    Modify the index.js according to the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { sessionService } from 'redux-react-session';
+import { AppContainer } from 'react-hot-loader';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import includes from 'lodash/includes';
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import configureStore from './store/configureStore';
+import App from './components/App';
+import locales from './locales';
+import { SUPPORTED_LANGUAGES, DEFAULT_LANGUAGE } from './constants/constants';
+import './styles/styles.scss';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+require('./icons/favicon.ico'); // Tell webpack to load favicon.ico
+
+// Fix for browsers that don't implement Intl by default e.g.: Safari)
+if (!window.Intl) {
+  require.ensure([
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+    'intl/locale-data/jsonp/es.js',
+  ], (require) => {
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    require('intl/locale-data/jsonp/es.js');
+  });
+}
+
+addLocaleData([...en, ...es]);
+const usersLocale = navigator.language.split('-')[0];
+const supportedUserLocale = includes(SUPPORTED_LANGUAGES, usersLocale);
+const locale = supportedUserLocale ? usersLocale : DEFAULT_LANGUAGE;
+const messages = locales[locale];
+
+const store = configureStore();
+
+sessionService.initSessionService(store);
+
+const renderApp = (Component) => {
+  render(
+    <IntlProvider
+      locale={locale}
+      messages={messages}
+      defaultLocale="en"
+    >
+      <Provider store={store}>
+        <AppContainer>
+          <Component />
+        </AppContainer>
+      </Provider>
+    </IntlProvider>,
+    document.getElementById('app')
+  );
+};
+
+renderApp(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    renderApp(App);
+  });
+}
